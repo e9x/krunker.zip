dc973adaa8c9e1ba54348a995e5dffd33f03e7c9
David Reed <reedswork@proton.me>
2023-04-16 23:24:40 -0400

    weird metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,8 +30,8 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`Krunker ${siteConfig.title}`}
+      description="A Krunker cheat."
     >
       <HomepageHeader />
     </Layout>
