38a2ade1b0171524d66a3d42031da5e721dc2b66
David Reed <reedswork@proton.me>
2023-04-17 15:20:45 -0400

    fix editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,8 +41,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/e9x/sketch-docs/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/e9x/sketch-docs/tree/master/",
         },
         blog: false,
         theme: {
